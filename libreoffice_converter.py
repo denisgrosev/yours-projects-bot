d8a807a42b33c8d00e58a05275d56b4e5a98af18
denisgrosev <grosevdenis143@gmail.com>
2025-06-16 19:59:16 +0300

    Update libreoffice_converter.py

--- libreoffice_converter.py
+++ libreoffice_converter.py
@@ -1,10 +1,23 @@
 import subprocess
 import os
+import shutil
 
 def convert(input_path, output_path=None):
+    # Попробуем найти soffice
+    soffice_path = shutil.which("soffice")
+    if not soffice_path:
+        # Попробуем дефолтный путь для Windows
+        default_path = r"C:\Program Files\LibreOffice\program\soffice.exe"
+        if os.path.exists(default_path):
+            soffice_path = default_path
+        else:
+            raise FileNotFoundError(
+                "Не найден исполняемый файл LibreOffice (soffice.exe). "
+                "Добавь LibreOffice в PATH или укажи абсолютный путь в libreoffice_converter.py"
+            )
     output_dir = os.path.dirname(output_path) if output_path else os.path.dirname(input_path)
     subprocess.run([
-        "libreoffice", "--headless", "--convert-to", "pdf",
+        soffice_path, "--headless", "--convert-to", "pdf",
         "--outdir", output_dir, input_path
     ], check=True)
 
